65c6bce9317a7870ea5b3177f410d89cbe6af82c
akshay-ghelani <akshayghelani627@gmail.com>
2025-03-17 15:39:03 +0530

    crud operation

---
+++ script.js
@@ -0,0 +1,26 @@
+// var selectRow = null;
+
+// //show alert
+// function showAlert(message, className){
+//     const div = document.createElement("div")
+//     div.className= `alert alert-${className}`;
+//     div.appendChild(document.createTextNode(message));
+//     const container = document.querySelector(".container");
+//     const main=document.querySelector(".main");
+//     container.insertBefore(div, main);
+//     setTimeout(()=> document.querySelector(".alert").remove(), 3000);
+// }
+
+// // delete data
+
+// document.querySelector("#student-list").addEventListener("click",(e)=>{
+//     alert();
+//     target = e.target;
+//     if(target.classList.contains("delete")){
+//         target.parentElement.parentElement.remove();
+//         showAlert("student data delete", "danger");
+//     }
+// });
+
+
+
